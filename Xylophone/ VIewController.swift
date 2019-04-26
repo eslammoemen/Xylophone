ab842f3c444c6e4ef5d23cccd6b5acdfb2c96689
Eslam Mohamed Moemen <eslammoemen.headway.contact@gmail.com>
2019-04-26 09:20:07 +0200

    first & last Commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var audioArray = ["note1","note2","note3","note4","note5","note6","note7",]
+    
+    var player: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func playSound(_ name : String ) {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+       playSound(audioArray[sender.tag-1])
+       
+    
+    }
+        
+        
+        
+    }
